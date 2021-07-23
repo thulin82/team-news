f249deccb2c6cc226531d0ec8120d69b02548cc1
Markus Thulin <macky_b@hotmail.com>
2021-07-23 08:57:41 +0200

    Add tournament info in search result

--- js/ui.js
+++ js/ui.js
@@ -5,11 +5,12 @@ class UI {
     }
 
     populateSearchResult(teams) {
+        this.searchResult.innerHTML = '';
         // Only take top 3
         for (let index = 0; index < 3; index++) {
             this.searchResult.innerHTML += `<input type="radio" id="html" name="team_radiobutton" 
                                             value="${teams.teams.teams[index].name}, ${teams.teams.teams[index].id}">
-                                            <label for="html">${teams.teams.teams[index].name}, ${teams.teams.teams[index].id}</label><br>`
+                                            <label for="html">${teams.teams.teams[index].name}, ${teams.teams.teams[index].primaryUniqueTournament.name} - ${teams.teams.teams[index].id}</label><br>`
         }
     }
 
