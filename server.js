68acf17ef9b095a660e75ed0f139e2aa2bfc798d
Markus Thulin <macky_b@hotmail.com>
2021-07-29 09:40:41 +0200

    Started to implement backend/frontend solution, things will not be working for a couple of commits

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const hbs = require('hbs');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: './config/config.env' });
+
+hbs.registerHelper("prettifyDate", function(timestamp) {
+    const options = {year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(timestamp*1000).toLocaleDateString('en-GB', options);
+});
+
+hbs.registerHelper("isNotNull", function(value) {
+    return value !== null;
+});
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.set('view engine', 'hbs');
+app.set("views", __dirname +"/views/layouts/");
+hbs.registerPartials(__dirname+ "/views/partials/");
+
+app.get('/', (req, res) => {
+    res.render('main');
+});
+
+app.get('/squad', (req, res) => {
+    res.render('squad');
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+//app.use('/', require('./routes/index'));
+
+const PORT = process.env.PORT || 4567;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
